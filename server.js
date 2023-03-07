78a8bc7a88f3afb4aa5a95a7db667086c4704c15
Collin Park <oliveguy85@gmail.com>
2023-03-06 23:50:11 -0700

    project structure init

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
-const express = express.get();
+const express = require('express');
+const app = express();
+app.set('view engine', 'ejs');
+
+app.get('/', (req,res)=>{
+  res.send('PetLink homepage');
+})
+
+app.listen(8080, ()=>{
+  console.log('listening on 8080')
+})
