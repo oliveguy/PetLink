b74ef009111116fffbf78c53334511aaf2ec39a6
Collin Park <oliveguy85@gmail.com>
2023-04-12 15:41:58 -0600

    Permission solved

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,15 @@ import './css/index_style.css';
 import './css/pages.css';
 import './css/style.css';
 
+import chat from './src-img/chat.png';
+import arrow from './src-img/down_arrow_.png';
+import heart from './src-img/heart.png';
+import more from './src-img/moreHome.png';
+import report from './src-img/report.png';
+import person from './src-img/person.png';
+import symbol from './src-img/PetLink-symbol.svg';
+import wordmark from './src-img/PetLink-wordmark-red.svg';
+
 function App() {
   let navigate = useNavigate();
   return (
@@ -21,10 +30,14 @@ function App() {
         <Route path="/home" element={<Home/>}>
           <Route path="welcome" element={<Welcome/>}/>
           <Route path="profile" element={<Profile/>}/>
+          <Route path="event" element={<Event/>}/>
+          <Route path="match" element={<Match/>}/>
+          <Route path="info" element={<Info/>}/>
         </Route>
       </Routes>
     </div>
   );
+
   function Index(){
     return(
       <div id="splash_main">
@@ -52,7 +65,7 @@ function App() {
   function Login(){
     return(
       <div>
-        <div class="form-container sign-in-container">
+        <div className="form-container sign-in-container">
           <form action="#">
             <h1 className="LS-title">Log In</h1>
             <input type="text" placeholder="Your email" />
@@ -68,7 +81,7 @@ function App() {
   function Signup(){
     return(
       <div>
-        <div class="form-container sign-in-container">
+        <div className="form-container sign-in-container">
           <form action="#">
             <h1 className="LS-title">Sign up</h1>
             <input type="text" placeholder="Your email" />
@@ -123,45 +136,45 @@ function App() {
         <label for="file" className='photoUpload'>&#10149; Upload Your Pet Photo</label>
         <input type="file" name="pet_photo" accept="image/*" id="file"/>
       
-      <input onClick={()=>{navigate('/home')}}type="submit" value="Save Information" />
+      <input onClick={()=>{navigate('/home/welcome')}}type="submit" value="Save Information" />
 
       </div>
     )
   }
+
   function Home(){
     return(
       <div className='homeBody'>
         <header className='home'>
-          <img src="/img/icons/down_arrow_.png" className="arrowBack" alt="back-arrow" onClick={()=>{navigate(-1)}}style={{transform: 'rotate(90deg)'}}/>
+          <img src={arrow} className="arrowBack" alt="back-arrow" onClick={()=>{navigate(-1)}}style={{transform: 'rotate(90deg)'}}/>
           <div>
-            <img src="/img/logo/PetLink-symbol.svg" className="logoHomeS" alt="petlink logo" onClick={()=>{navigate("/home")}}/>
-            <img src="/img/logo/PetLink-wordmark-red.svg" className="logoHomeT" alt="petlink logo" onClick={()=>{navigate("/home")}}/>
+            <img src={symbol} className="logoHomeS" alt="petlink logo" onClick={()=>{navigate("/home/welcome")}}/>
+            <img src={wordmark} className="logoHomeT" alt="petlink logo" onClick={()=>{navigate("/home/welcome")}}/>
           </div>
-          <img src="img/icons/moreHome.png" className="more" alt="more" onClick={()=>{navigate("/home")}} />
+          <img src={more} className="more" alt="more" onClick={()=>{navigate("/home/welcome")}} />
         </header>
         <Outlet></Outlet>
-        {/* <Welcome></Welcome> */}
-        {/* <Profile></Profile> */}
+
         <nav className='HomeNav'>
           <ul>
             <li>
               <a>
-                <img src="img/icons/person.png" onClick={()=>{navigate('/home/profile')}} alt="personal information"/>
+                <img src={person} onClick={()=>{navigate('/home/profile')}} alt="personal information"/>
               </a>
             </li>
             <li>
               <a>
-                <img src="img/icons/chat.png" alt="Communication"/>
+                <img src={chat} onClick={()=>{navigate('/home/event')}} alt="Communication"/>
               </a>
             </li>
             <li>
               <a>
-                <img src="img/icons/heart.png" alt="Matching "/>
+                <img src={heart} onClick={()=>{navigate('/home/match')}} alt="Matching "/>
               </a>
             </li>
             <li>
               <a>
-                <img src="img/icons/report.png" alt="info"/>
+                <img src={report} onClick={()=>{navigate('/home/info')}} alt="info"/>
               </a>
             </li>
           </ul>
@@ -171,18 +184,41 @@ function App() {
   }
   function Welcome(){
     return(
-      <div className='welcome'>
+      <div className='homeComponent welcome'>
         <p>WELCOME</p>
       </div>
     )
   }
   function Profile(){
     return (
-      <div>
+      <div className='homeComponent profile'>
         <p>Profile</p>
+      </div>
+    )
+  }
+  function Event(){
+    return (
+      <div className='homeComponent event'>
+        <p>Event</p>
+      </div>
+    )
+  }
+  function Match(){
+    return (
+      <div className='homeComponent match'>
+        <p>Match</p>
+      </div>
+    )
+  }
+  function Info(){
+    return (
+      <div className='homeComponent info'>
+        <p>Info</p>
       </div>
     )
   }
 }
 
 export default App;
+// Permision Denied Issue(MAC)
+// sudo chmod +x node_modules/.bin/react-scripts
